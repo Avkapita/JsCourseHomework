b2896424901793068251a1be9df4499114404a2a
Anna <avkapita@gmail.com>
2019-02-18 09:25:06 +0300

    1,2,4

--- src/indexhm02.js
+++ src/indexhm02.js
@@ -7,8 +7,9 @@
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
 function forEach(array, fn) {
-	for (let i=0; i<array.length; i++) //перебираем эелемнты массива
-		const result = fn(array[i], i, array);// для каждой итерации вызываем функцию fn и присваиваем значение переменной result;
+	for (var i = 0; i < array.length; i++) // перебираем элементы массива
+		var result = fn(array[i], i, array);
+		return result; // для каждой итерации вызываем функцию fn и присваиваем значение переменной result;
 	}
 
 /*
@@ -17,13 +18,21 @@ function forEach(array, fn) {
  Напишите аналог встроенного метода map для работы с массивами
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
-function map(array, fn) {
-	for (var i=0; i < array.length; i++)
-		var result = fn(array[i]);
-	var resultArray;
-	resultArray.push(result);
+/*var array=[1,2,3,4,5]; 
+function fn(a) {
+	var sum = a*2;
+	return sum;
 };
+var resultArray = [];
+map(array, fn);*/
 
+function map(array, fn) {
+	for (var i = 0; i < array.length; i++) {
+		
+		resultArray[i] = fn(array[i]);
+	}
+	return resultArray;
+}
 
 /*
  Задание 3:
@@ -31,8 +40,31 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
+ /*var arr=[1,2,3,4,5]; 
+function fn(prevValue, array[i], i, array) {
+	var res = array[i]+prevValue;
+	console.log(res);
+	return res;
+};
+reduce(arr, fn, 2);*/
+
+
+/*
 function reduce(array, fn, initial) {
-}
+	if (!initial) {
+		prevValue = array[0];
+		for (var i=1; i<array.length; i++) {
+			fn(prevValue, array[i], i, array) {};
+			console.log(res);
+		}
+	} else {
+		prevValue = initial;
+		for (var i = 0; i<array.length; i++) {
+			fn(prevValue, array[i], i, array) {};
+			console.log(res);
+		}
+	}
+*/
 
 /*
  Задание 4:
@@ -42,8 +74,20 @@ function reduce(array, fn, initial) {
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
+/*    var a = { 
+    	name: 'Сергей', 
+    	lastName: 'Петров'};
+    */
 function upperProps(obj) {
-}
+	var resultArray = [];
+		for (var key in obj) {
+			var result = key.toUpperCase();
+          	resultArray.push(result);
+      	}
+	return resultArray;
+	} 
+/*upperProps(a);*/
+
 
 /*
  Задание 5 *:
