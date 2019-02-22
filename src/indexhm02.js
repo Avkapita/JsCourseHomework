be684d7ad56683ebff569840d1493f8fd2c0fcc7
Anna <avkapita@gmail.com>
2019-02-22 14:52:47 +0300

    3

--- src/indexhm02.js
+++ src/indexhm02.js
@@ -45,34 +45,46 @@ map(array, fn);
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
+ //const array = [1, 2, 3, 4];//создан массив
 
-function fnr(prevValue) {
+var result, curValue, initialValue, prevValue;//объявлены переменные
 
+
+function reducer(array, fn, initialValue) {//функция reducer объявляется с параметрами
+	if (initialValue) {// если передан как аргумент initialValue
+		prevValue = initialValue;//предыдущее значение равно initialValue.тогда curvalue будет следующее по массиву
 			for (var i = 0; i < array.length; i++) {
-              console.log(curItem,prevValue);
-				curItem = array[i + 1];
-               console.log(curItem,prevValue);
-            	prevValue = prevValue + curItem;
-               console.log(curItem,prevValue);
-     		}
-			return prevValue;
+				curValue = array[i];
+				console.log(result);
+				fn(prevValue, array, i);
+		return result;
 		}
-
-function reducing(initialValue, array, fnr) {
-   console.log(array);
-	if (initialValue) {
-		prevValue = initialValue;
-		
-		fnr();
-	} else {
-		prevValue = array[0];
-
-		fnr();
+	} else {// если не передан initialValue
+		prevValue = array[0];//prevValue равен элементу с индексом 0 в массиве, а значит 1.
+      
+		for (var i = 1; i < array.length; i++) {
+			curValue = array[i];
+			console.log(result, curValue,prevValue, i);
+			fn(prevValue, array, i);
+			 //вызываем функцию сложения элементов - для каждого элемента массива с предыдущим значением.
+			//на первой итерации prevValue=1, array[1] = 2. складываем, получаем 3. 3 записываем в prevValue.
+			//на второй итерации prevValue=3, array[2] = 3. складываем, получаем 6, записываем в prevValue
+			// на третей - prevValue = 6, array[3]=4. складываем, получаем 10.записываем в prevValue
+			//условие дальше не срабатывает
+		}
+		return result;
 	} 
+};
+
+function fn(prevValue, curValue, array, i) {
+	console.log(result, curValue,prevValue, i);
+			result = curValue + prevValue;
+			prevValue = result;
+			console.log(result, curValue,prevValue, i);
+			return result;
 }
 
-var arr = [1, 2, 3, 4, 5];
-reducing(0, (...arr), fnr);
+reducer(array, fn);//вызываем функцию reducer и записываем результат вызова в result
 
 /*
  Задание 4:
